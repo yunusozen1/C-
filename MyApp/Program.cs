e20c53d18cd4c28b92cce839a07b32eca14294f3
emre ozen <yunusozen1121@gmail.com>
2023-08-30 20:45:48 +0300

    C#

---
+++ MyApp/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("İsmini gir:");
+string name = Console.ReadLine();
+Console.WriteLine("Soyismini gir:");
+string surname= Console.ReadLine();
+Console.WriteLine(name+" "+surname);
