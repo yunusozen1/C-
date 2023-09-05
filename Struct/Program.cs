2566d2a159000edb5e2d0dd459c70b468b1890fb
emre ozen <yunusozen1121@gmail.com>
2023-09-05 19:46:20 +0300

    Struct

---
+++ Struct/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Struct{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen= new Dikdortgen();
+            dikdortgen.KisaKenar=3;
+            dikdortgen.UzunKenar=4;
+
+            Console.WriteLine("Class alan hesabı: {0}",dikdortgen.AlanHesapla());
+            Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct(3,4);
+            
+            Console.WriteLine("Class alan hesabı: {0}",dikdortgen_struct.AlanHesapla());
+        }
+    }
+
+    public class Dikdortgen{
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen(){
+            KisaKenar=3;
+            UzunKenar=4;   
+        }
+        
+        public long AlanHesapla(){
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    public struct Dikdortgen_Struct{
+        public int KisaKenar;
+        public int UzunKenar;
+        public Dikdortgen_Struct(int kisaKenar,int uzunKenar){
+            KisaKenar=kisaKenar;
+            UzunKenar=uzunKenar;
+        }
+
+        public long AlanHesapla(){
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+    
+    
+
+}
+
